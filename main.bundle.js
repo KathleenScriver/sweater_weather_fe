b76e13364a1d6e0b592b5da57f7a52b185400bdc
Kathleen Yruegas <kathleenecn@gmail.com>
2018-12-03 15:01:43 -0700

    Runs build

--- main.bundle.js
+++ main.bundle.js
@@ -44,8 +44,96 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-	// This file is in the entry point in your webpack config.
-	"use strict";
+	'use strict';
+
+	$('#change-location-popup').hide();
+
+	$('#change-location-button').on('click', function () {
+	  $('#change-location-popup').show();
+	  $('#location-input').focus();
+	});
+
+	$('#cancel').on('click', function () {
+	  return $('#change-location-popup').hide();
+	});
+
+	var getForecast = function getForecast() {
+	  var location = $('#location-input').val() ? $('#location-input').val() : "Denver, CO";
+	  localStorage.setItem('location', location);
+	  getImage();
+
+	  fetch('https://sweater-weather-ky.herokuapp.com/api/v1/forecast?location=' + location).then(function (response) {
+	    return response.json();
+	  }).then(function (forecast) {
+	    return displayForecast(forecast);
+	  }).catch(function (error) {
+	    return console.error({ error: error });
+	  });
+
+	  $('#change-location-popup').hide();
+	  $('#city-state').text(location);
+	};
+
+	var getImage = function getImage() {
+	  var imageLocation = localStorage.getItem('location');
+
+	  fetch('https://sweater-weather-ky.herokuapp.com/api/v1/background?location=' + imageLocation).then(function (response) {
+	    return response.json();
+	  }).then(function (imageData) {
+	    return setImage(imageData);
+	  }).catch(function (error) {
+	    return console.error({ error: error });
+	  });
+	};
+
+	var setImage = function setImage(imageData) {
+	  var imagePath = imageData.data.attributes.location_image;
+	  $('body').css('background-image', 'url(' + imagePath + ')');
+	};
+
+	var displayForecast = function displayForecast(forecast) {
+	  $('#location-input').val('');
+	  var forecastData = forecast.data.attributes;
+	  setLocationNowSection(forecastData);
+	  // setDetailsSection();
+	  setHourlySection(forecastData.hourly_temps.data);
+	  // setDailySection();
+	};
+
+	var setLocationNowSection = function setLocationNowSection(daily_attributes) {
+	  setTimeDate(daily_attributes.current_weather.data.attributes.time);
+	  setNowForecast(daily_attributes);
+	};
+
+	var setNowForecast = function setNowForecast(attributes) {
+	  var dailyData = attributes.current_weather.data.attributes;
+	  $('#now-description .text').text(dailyData.summary);
+	  $('#location-now-icon').attr('src', 'assets/' + dailyData.icon + '.png');
+	  $('#now-temp .text').text(Math.round(dailyData.temperature));
+	  $('#now-high-number').text(Math.round(attributes.daily_weather.data[0].attributes.temp_high));
+	  $('#now-low-number').text(Math.round(attributes.daily_weather.data[0].attributes.temp_low));
+	};
+
+	var setTimeDate = function setTimeDate(unixTime) {
+	  var dayTime = moment.unix(unixTime);
+	  var formattedDay = dayTime.format("hh:mm a, M/D");
+	  $('#time-date').text(formattedDay);
+	};
+
+	var setHourlySection = function setHourlySection(hourlyData) {
+	  $('#hourly-forecast-section').empty();
+	  for (var i = 0; i < 9; i++) {
+	    var temp = Math.round(hourlyData[i].attributes.temperature);
+	    var time = hourlyData[i].attributes.time;
+	    $('#hourly-forecast-section').append('<div><p class=\'time\'>' + time + '</p><p class=\'temp\'>' + temp + '<img src=\'assets/circle-shape-outline.png\', alt=\'degree symbol\' class=\'md-deg-symbol\'></p></div>');
+	  };
+	};
+
+	getForecast();
+
+	$('#photo-button').on('click', getImage);
+
+	$('#location-set-button').on('click', getForecast);
 
 /***/ })
 /******/ ]);
